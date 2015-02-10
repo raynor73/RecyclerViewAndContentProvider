f327553c50c5a5f08e6e3a96afdeb81f937e7abb
Igor Lapin <ilapin@parallels.com>
2015-02-10 16:10:24 +0300

    Implement file access in content provider

--- app/src/main/java/ru/ilapin/recyclerviewandcontentprovider/Providers/CitiesContentProvider.java
+++ app/src/main/java/ru/ilapin/recyclerviewandcontentprovider/Providers/CitiesContentProvider.java
@@ -10,10 +10,15 @@ package ru.ilapin.recyclerviewandcontentprovider.providers;
 import android.content.ContentProvider;
 import android.content.ContentValues;
 import android.content.UriMatcher;
+import android.content.res.AssetFileDescriptor;
 import android.database.Cursor;
+import android.database.MatrixCursor;
 import android.net.Uri;
+import android.provider.OpenableColumns;
 import android.util.Log;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Arrays;
 
 import ru.ilapin.recyclerviewandcontentprovider.database.DatabaseHelper;
@@ -25,6 +30,9 @@ public class CitiesContentProvider extends ContentProvider {
 	private static final int CITIES = 0;
 	private static final int CAPITALS = 1;
 	private static final int CITY = 2;
+	private static final int IMAGE = 3;
+
+	private static final String IMAGE_FILENAME = "image.jpg";
 
 	private DatabaseHelper mDatabaseHelper;
 	private final UriMatcher mUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
@@ -37,6 +45,7 @@ public class CitiesContentProvider extends ContentProvider {
 		mUriMatcher.addURI(CitiesContract.AUTHORITY, "cities", CITIES);
 		mUriMatcher.addURI(CitiesContract.AUTHORITY, "capitals", CAPITALS);
 		mUriMatcher.addURI(CitiesContract.AUTHORITY, "cities/#", CITY);
+		mUriMatcher.addURI(CitiesContract.AUTHORITY, "image", IMAGE);
 
 		return true;
 	}
@@ -48,7 +57,7 @@ public class CitiesContentProvider extends ContentProvider {
 		switch (mUriMatcher.match(uri)) {
 			case CITIES: {
 				Log.d(TAG, "cities URI match");
-				Cursor cursor = mDatabaseHelper.getReadableDatabase().query(
+				final Cursor cursor = mDatabaseHelper.getReadableDatabase().query(
 						"City",
 						projection,
 						selection,
@@ -83,9 +92,9 @@ public class CitiesContentProvider extends ContentProvider {
 						sortOrder
 				);
 
-			case CITY:
+			case CITY: {
 				Log.d(TAG, "city URI match");
-				Cursor cursor = mDatabaseHelper.getReadableDatabase().query(
+				final Cursor cursor = mDatabaseHelper.getReadableDatabase().query(
 						"City",
 						projection,
 						CitiesContract.Cities._ID + " = ?",
@@ -96,6 +105,28 @@ public class CitiesContentProvider extends ContentProvider {
 				);
 				cursor.moveToFirst();
 				return cursor;
+			}
+
+			case IMAGE: {
+				Log.d(TAG, "image URI match");
+				final MatrixCursor cursor = new MatrixCursor(new String[]{
+					OpenableColumns.DISPLAY_NAME,
+					OpenableColumns.SIZE
+				}, 1);
+
+				try {
+					final AssetFileDescriptor fileDescriptor = getContext().getAssets().openFd(IMAGE_FILENAME);
+					cursor.addRow(new String[]{
+							IMAGE_FILENAME,
+							String.valueOf(fileDescriptor.getLength())
+					});
+					fileDescriptor.close();
+				} catch (final IOException e) {
+					throw new RuntimeException(e);
+				}
+
+				return cursor;
+			}
 
 			default:
 				Log.d(TAG, "no URI match");
@@ -107,7 +138,16 @@ public class CitiesContentProvider extends ContentProvider {
 	public String getType(final Uri uri) {
 		Log.d(TAG, "getType: " + uri);
 
-		return mUriMatcher.match(uri) == CITY ? CitiesContract.Cities.CONTENT_ITEM_TYPE : CitiesContract.Cities.CONTENT_TYPE;
+		switch (mUriMatcher.match(uri)) {
+			case IMAGE:
+				return "image/jpeg";
+
+			case CITY:
+				return CitiesContract.Cities.CONTENT_ITEM_TYPE;
+
+			default:
+				return CitiesContract.Cities.CONTENT_TYPE;
+		}
 	}
 
 	@Override
@@ -128,10 +168,25 @@ public class CitiesContentProvider extends ContentProvider {
 	public int update(final Uri uri, final ContentValues values, final String selection, final String[] selectionArgs) {
 		Log.d(TAG, "update: " + uri);
 
-		int affectedRows = mDatabaseHelper.getWritableDatabase().update("City", values, selection, selectionArgs);
+		final int affectedRows = mDatabaseHelper.getWritableDatabase().update("City", values, selection, selectionArgs);
 
 		getContext().getContentResolver().notifyChange(uri, null);
 
 		return affectedRows;
+	}
+
+	@Override
+	public AssetFileDescriptor openAssetFile(final Uri uri, final String mode) throws FileNotFoundException {
+		Log.d(TAG, "openFile: " + uri);
+
+		if (mUriMatcher.match(uri) != IMAGE) {
+			throw new FileNotFoundException(uri.toString());
+		}
+
+		try {
+			return getContext().getAssets().openFd(IMAGE_FILENAME);
+		} catch (final IOException e) {
+			throw new RuntimeException(e);
+		}
 	}
 }
